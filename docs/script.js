cff005fa4877c0f230764d22e63d590a296b8279
mspivak98 <87453518+mspivak98@users.noreply.github.com>
2021-08-01 17:33:35 -0400

    Add files via upload

--- docs/script.js
+++ docs/script.js
@@ -3,43 +3,40 @@ let score=10;//original score
 let highscore=0;//you can update this when you have other highscore
 document.querySelector('.score').textContent=`💰Score:${score}`
 document.querySelector('.highscore').textContent=`🥇Highscore:${highscore}`
-console.log(randomnumber)
 document.querySelector('.Check').addEventListener('click',function(){
    inputUser=Number(document.querySelector('.userInput').value)
    if(!inputUser){
     document.querySelector('.msg').textContent='No Input!!'
    }//This will give a message if the user did not submit a value
    if (randomnumber==inputUser){
-      document.querySelector('.msg').textContent='You got the right answer!!';
+      document.querySelector('.title').textContent='You got the right answer!!';
+      document.querySelector(".think").src="images/correct.gif";
       document.querySelector('.guessnum').textContent=`${randomnumber}`
       document.querySelector('.highscore').textContent=`🥇Highscore:${score}`
-      document.body.className = "green"
+      document.body.className = "green";
    }
    else if (randomnumber !== inputUser && (score >=1)){
      score=score-1
      document.querySelector('.score').textContent=`💰Score:${score}`
+     document.querySelector(".think").src="images/wrong.gif";
      if (score===0) {
-       document.querySelector('.msg').textContent='Game Over!!';
-       document.body.className = "red"
+       document.querySelector('.title').textContent='Game Over!!';
+       document.querySelector('.guessnum').textContent=`Answer:${randomnumber}`
+       document.querySelector(".think").src="images/go.gif";
+       document.body.className = "red";
      }
-     else if (randomnumber>inputUser) {
-       document.querySelector('.msg').textContent='↟Try a greater number!!';
-     }
-     else if (randomnumber<inputUser) {document.querySelector('.msg').textContent='⇓Try a smaller number!!'
-
-     }
+     else if (randomnumber>inputUser) {document.querySelector('.msg').textContent='↟Try a greater number!!';}
+     else if (randomnumber<inputUser) {document.querySelector('.msg').textContent='⇓Try a smaller number!!'}
    }
 
 })
 document.querySelector('.resetbtn').addEventListener('click',function(){
    randomnumber = Math.trunc(Math.random()*100)+1
    score = 10;
+   document.querySelector('.title').textContent='Guess My Number!';
+   document.querySelector('.guessnum').textContent=`?`;
    document.querySelector('.score').textContent=`💰Score:${score}`
-   console.log(randomnumber)
    document.body.className = "default"
+   document.querySelector(".think").src="images/thinker.gif";
+   document.querySelector('.guessnum').textContent=`?`
 })
-// You are listening to user input. You can define the function right away of outside
-
-/*to change background color
-
-document.querySelector('body').style.backgroundColor=''*/
